dde8593ce465866af76ae6edd38531c144b90a22
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-06-01 18:38:39 -0300

    Update 1-hbnb_route.py

--- 0x09-python-web_framework/1-hbnb_route.py
+++ 0x09-python-web_framework/1-hbnb_route.py
@@ -1,4 +1,6 @@
-pt to starts a Flask web Application"""
+#!/usr/bin/python3
+
+"""Script to starts a Flask web Application"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
